bffa55b42f466c86b21236ce573f9bbc73974bb7
Max Sadler <maxsadler@Maxs-MacBook-Air.local>
2022-05-16 15:28:51 -0400

    Working on game sessions

--- backend/Client/client.js
+++ backend/Client/client.js
@@ -1,7 +1,17 @@
-import { io }from 'socket.io-client'
-
+import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
 const socket = io('http://localhost:3000')
+const btn = document.getElementById('join')
+const roomID = document.getElementById('roomID')
+
+btn.addEventListener('click', joinMatch)
 
 socket.on('connect', () => {
     console.log("Connected to server ")
+    console.log(socket.id)
 })
+
+function joinMatch(){
+    socket.emit('join-match', roomID.value, (joined) => {
+        console.log(joined)
+    })
+}
--- backend/Server/index.js
+++ backend/Server/index.js
@@ -1,7 +1,24 @@
 const { Server } = require('socket.io')
-const io = new Server(3000)
+
+const io = new Server(3000, {
+    cors:{
+        origin: "http://127.0.0.1:5500"
+    }
+})
+
+const rooms = io.of("/").adapter.rooms;
 
 io.on('connection', socket => {
-    console.log("A client has connected")
+    console.log(`A client has connected - ${socket.id}`)
+
+    socket.on('join-match', (roomID, cb) => {
+        if (rooms.has(roomID)){
+            socket.join(roomID)
+            cb(true)
+        }
+        else{
+            cb(false)
+        }
+    })
 })
 
